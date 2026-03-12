dfa56885e0d00f6f06f8225c0a2cd624421d5b77
TreasuredOwl <people_life@live.ru>
2026-03-13 00:08:13 +0200

    stable

--- login/GameServerRegister.sh
+++ login/GameServerRegister.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Djava.util.logging.config.file=console.cfg -cp ./../libs/*: com.l2journey.tools.gsregistering.GameServerRegister -c
+java -Djava.util.logging.config.file=console.cfg -cp ./../libs/*: com.l2journey.tools.gsregistering.GameServerRegister
